c073121e9c9ed925a8e33538596569cc413c8019
Bill Shawn <bbao20004856@gmail.com>
2024-12-03 22:16:03 -0800

    set up the personal nasa api key

--- app/src/main/java/com/zybooks/workingwithdata/NasaAPI.kt
+++ app/src/main/java/com/zybooks/workingwithdata/NasaAPI.kt
@@ -7,8 +7,6 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
